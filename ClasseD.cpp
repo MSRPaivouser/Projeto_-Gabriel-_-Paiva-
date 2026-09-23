8f2a4265406a1bdfd320dcb83be9a888edfb15fb
Gabriel Duarte Paiva <gabrielduartepaiva07@gmail.com>
2026-09-22 22:40:49 -0300

    Criação da Classe D

---
+++ ClasseD.cpp
@@ -0,0 +1,14 @@
+#include "ClasseD.h"
+#include <iostream>
+
+ClasseD::ClasseD() : D1(0), D2("") {}
+ClasseD::ClasseD(int d1, std::string d2) : D1(d1), D2(d2) {}
+
+int ClasseD::getD1() const { return D1; }
+std::string ClasseD::getD2() const { return D2; }
+
+void ClasseD::setD1(int d1) { D1 = d1; }
+void ClasseD::setD2(std::string d2) { D2 = d2; }
+
+void ClasseD::MD1() { std::cout << "MD1" << std::endl; }
+void ClasseD::MD2() { std::cout << "MD2" << std::endl; }
---
+++ ClasseD.h
@@ -0,0 +1,25 @@
+#ifndef CLASSED_H
+#define CLASSED_H
+
+#include <string>
+
+class ClasseD {
+private:
+    int D1;
+    std::string D2;
+
+public:
+    ClasseD();
+    ClasseD(int d1, std::string d2);
+
+    int getD1() const;
+    std::string getD2() const;
+
+    void setD1(int d1);
+    void setD2(std::string d2);
+
+    void MD1();
+    void MD2();
+};
+
+#endif
